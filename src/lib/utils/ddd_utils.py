3D BBox: fix projection and object rotation

src/lib/utils/ddd_utils.py
@@ -5,18 +5,54 @@ from os import DirEntry
 
 import numpy as np
 import cv2
+import math
 
-def compute_box_3d(dim, location, rotation_y):
+# Calculates Rotation Matrix given euler angles.
+def eulerAnglesToRotationMatrix(theta) :
+
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])
+
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+
+    return R
+
+  
+def rotationMatrixToEulerAngles(R) :
+    yaw=math.atan2(R[1,0], R[0,0])
+    pitch=math.atan2(-R[2,0], math.sqrt(R[2,1]**2+R[2,2]**2))
+    roll=math.atan2(R[2,1], R[2,2])
+    return pitch, yaw, roll
+
+
+def compute_box_3d(dim, location, rotation_y, R=None):
   print("compute_box_3d")
   # dim: 3
   # location: 3
   # rotation_y: 1
   # return: 8 x 3
   c, s = np.cos(rotation_y), np.sin(rotation_y)
-  R = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]], dtype=np.float32)
-  with open("/home/morro/RTM3D/kitti_format/data/kitti/calib/000001.txt") as f:
-    r_txt = f.readlines()[4].strip().split()[1:]
-    R = np.array(r_txt, dtype=np.float32).reshape(3,3)
+
+  if R is None:
+    R = np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]], dtype=np.float32)
+  else:
+    # Apply object rotation_y, sum it to the camera angle rotation
+    pitch, yaw, roll = rotationMatrixToEulerAngles(R)
+    R1 = eulerAnglesToRotationMatrix((0, rotation_y + pitch, 0))
+    R = np.dot(R, R1)
+
   l, w, h = dim[2], dim[1], dim[0]
   x_corners = [l/2, l/2, -l/2, -l/2, l/2, l/2, -l/2, -l/2,0]
   y_corners = [0,0,0,0,-h,-h,-h,-h,-h/2]

src/tools/kitti.py
@@ -114,6 +114,11 @@ for SPLIT in SPLITS:
             else:
                 calib = read_clib(calib_path)
 
+            c = open(calib_path)
+            r_txt = c.readlines()[4].strip().split()[1:]
+            calib_R = np.array(r_txt, dtype=np.float32).reshape(3,3)
+            c.close()
+
             image_info = {'file_name': '{}.png'.format(line),
                           'id': int(image_id),
                           'calib': calib.tolist()}
@@ -142,7 +147,7 @@ for SPLIT in SPLITS:
                 if tmp[0] in det_cats:
                     image = cv2.imread(os.path.join(image_set_path, image_info['file_name']))
                     bbox = [float(tmp[4]), float(tmp[5]), float(tmp[6]), float(tmp[7])]
-                    box_3d = compute_box_3d(dim, location, rotation_y)
+                    box_3d = compute_box_3d(dim, location, rotation_y, calib_R)
                     box_2d_as_point,vis_num,pts_center = project_to_image(box_3d, calib,image.shape)
                     bbb_img = draw_box_3d(bbb_img, box_2d_as_point[:, :2].astype(np.float32))
                     box_2d_as_point=np.reshape(box_2d_as_point,(1,27))