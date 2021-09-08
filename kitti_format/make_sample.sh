7b64a78c294b32b8a150de47a08fc1ed8ec3e021
Moreno Razzoli <morrolinux@gmail.com>
2021-09-08 15:52:49 +0200

    make_sample script: fix appending to trainval

--- kitti_format/make_sample.sh
+++ kitti_format/make_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DS=~/Carla0.9.10-kitti-data-export/_out/training
-# DS=~/CARLA_DS_1/training
+# DS=~/Carla0.9.10-kitti-data-export/_out/training
+DS=~/CARLA_DS_1/training
 # DS=~/Kitti/object/training
 
 mkdir -p kitti_format/data/kitti
@@ -13,22 +13,22 @@ mkdir label
 mkdir planes
 mkdir annotations
 
-for i in {000000..000001}
+for i in {000000..000010}
 do 
     cp $DS/calib/$i.txt calib/
     cp $DS/image_2/$i.png image/
     cp $DS/label_2/$i.txt label/
     cp $DS/planes/$i.txt planes/
     
-    echo $i > trainval.txt
+    echo $i >> trainval.txt
 done
 
-for i in {000000..000001}
+for i in {000000..000008}
 do 
     echo $i >> train.txt
 done
 
-for i in {000000..000001}
+for i in {000008..000010}
 do 
     echo $i >> val.txt
 done
